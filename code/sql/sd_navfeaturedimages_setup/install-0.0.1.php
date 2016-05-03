642fc66c6a5c9f37a95c83bc8d79004d8ea33e30
reminagreenfield <reminagreenfield@gmail.com>
2016-05-03 12:27:38 -0400

    Add conditionals to php setup to prevent duplicate entries

--- code/sql/sd_navfeaturedimages_setup/install-0.0.1.php
+++ code/sql/sd_navfeaturedimages_setup/install-0.0.1.php
@@ -2,43 +2,49 @@
 $installer = $this;
 $installer->startSetup();
 
-$this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_image', array(
-    'type' => 'varchar',
-    'backend' => 'catalog/category_attribute_backend_image',
-    'label' => 'Navigation Featured Image',
-    'input' => 'image',
-    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-    'visible' => 1,
-    'default' => '',
-    'user_defined' => 1,
-    'required' => 0,
-    'sort_order' => 111,
-    'group' => 'General Information'
-));
+if (!$this->getAttributeId(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_image')) {
+    $this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_image', array(
+        'type' => 'varchar',
+        'backend' => 'catalog/category_attribute_backend_image',
+        'label' => 'Navigation Featured Image',
+        'input' => 'image',
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+        'visible' => 1,
+        'default' => '',
+        'user_defined' => 1,
+        'required' => 0,
+        'sort_order' => 111,
+        'group' => 'General Information'
+    ));
+};
 
-$this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_link', array(
-    'label' => 'Navigation Featured Link',
-    'input' => 'text',
-    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-    'visible' => 1,
-    'default' => '',
-    'user_defined' => 1,
-    'required' => 0,
-    'sort_order' => 112,
-    'group' => 'General Information'
-));
+if (!$this->getAttributeId(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_link')) {
+    $this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_link', array(
+        'label' => 'Navigation Featured Link',
+        'input' => 'text',
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+        'visible' => 1,
+        'default' => '',
+        'user_defined' => 1,
+        'required' => 0,
+        'sort_order' => 112,
+        'group' => 'General Information'
+    ));
+};
 
-$this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_text', array(
-    'label' => 'Navigation Featured Text',
-    'input' => 'text',
-    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-    'visible' => 1,
-    'default' => '',
-    'user_defined' => 1,
-    'required' => 0,
-    'sort_order' => 113,
-    'group' => 'General Information'
-));
+if (!$this->getAttributeId(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_text')) {
+    $this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'nav_featured_text', array(
+        'label' => 'Navigation Featured Text',
+        'input' => 'text',
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+        'visible' => 1,
+        'default' => '',
+        'user_defined' => 1,
+        'required' => 0,
+        'sort_order' => 113,
+        'group' => 'General Information'
+    ));
+};
 
 //mark category flat tables as req. reindex
 $process = Mage::getModel('index/indexer')->getProcessByCode('catalog_category_flat');
